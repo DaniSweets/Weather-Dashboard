dc5548ed6e332aa66afa42677a2b5242c0f62750
Dani <danisweetwood@gmail.com>
2024-02-23 03:15:31 -0600

    connected js and html

--- script.js
+++ script.js
@@ -4,7 +4,10 @@
 //api.openweathermap.org/data/2.5/forecast/daily?lat={lat}&lon={lon}&cnt=5&appid={API key}
 //passes in lat & lon, returns weather info
 
-//key: cdbb44dbf98708fea636f843b1b4d3b1
+const apiKey = "cdbb44dbf98708fea636f843b1b4d3b1";
+var cityName = document.getElementById("cityInput").value;
+var currentWeather = document.getElementById("currentContent");
+var forecast = document.getElementById("futureContent");
 
 //event handler for searchbar
 //functionality to take in info from searchbar and insert it into fetch request
