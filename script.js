62d54751208c2a6a008c2e37c2c2792997b3ca37
Dani <danisweetwood@gmail.com>
2024-02-23 16:27:36 -0600

    able to fetch and return

--- script.js
+++ script.js
@@ -8,33 +8,58 @@
 
 const apiKey = "cdbb44dbf98708fea636f843b1b4d3b1";
 
-var currentWeather = document.getElementById("currentContent");
-var forecast = document.getElementById("futureContent");
+window.addEventListener('load', (event) => {
+    console.log(document.querySelector("#testLorem"));
+    var currentWeather = document.getElementById("currentContent");
+    var forecast = document.getElementById("futureContent");
+    console.log(forecast);
+    console.log(currentWeather);
+});
 
 //event handler for searchbar
-document.getElementById("searchForm").addEventListener("submit", function (event) {
-    event.preventDefault();
-    //functionality to take in info from searchbar and insert it into fetch request
-    var cityName = document.getElementById("cityInput").value;
-    var fetchUrl = `http://api.openweathermap.org/data/2.5/forecast?q=${cityName}&limit=5&appid=${apiKey}`;
-    displayWeather(cityName, fetchUrl);
+window.addEventListener('load', (event) => {
+    document.getElementById("searchForm").addEventListener("submit", function (event) {
+        event.preventDefault();
+        //functionality to take in info from searchbar and insert it into fetch request
+        // var cityName = document.getElementById("cityInput").value;
+        // var fetchUrl = `http://api.openweathermap.org/data/2.5/forecast?q=${cityName}&limit=5&appid=${apiKey}&units=imperial`;
+        // displayWeather(cityName, fetchUrl);
+
+        var cityName = "denver";
+        var fetchUrl = "http://api.openweathermap.org/data/2.5/forecast?q=denver&appid=cdbb44dbf98708fea636f843b1b4d3b1&units=imperial";
+
+        displayWeather(cityName, fetchUrl);
+    });
 });
 
 //fetch request to return weather info
 //add in html elements to render with info returned
 function displayWeather(cityName, fetchUrl) {
     fetch(fetchUrl)
-    .then(function(response) { 
-        return response.json(); 
-    })
-    console.log(response);
-    currentWeather.innerHTML = `
+        .then(function (response) {
+            return response.json()
+        })
+        .then(function (data) {
+
+
+
+
+            var currentWeather = document.getElementById("currentContent");
+            var forecast = document.getElementById("futureContent");
+            // console.log(response);`
+            currentWeather.innerHTML = `
     <p class="subtitle">Current Weather in ${cityName}</p>
     <ul>
-    <li>Temperature: </li>
-    <li>Feels like </li>
-    <li>Low of </li>
-    <li>High of </li>
+    <li>Temperature: ${data.list[0].main.temp}°F</li>
+    <li>Feels like ${data.list[0].main.feels_like}°F</li>
+    <li>Low of ${data.list[0].main.temp_min}°F</li>
+    <li>High of ${data.list[0].main.temp_max}°F</li>
 </ul>
     `
-}
+        })
+};
+
+// let cityName = "denver";
+// let fetchUrl = "api.openweathermap.org/data/2.5/forecast?q=denver&appid=cdbb44dbf98708fea636f843b1b4d3b1&units=imperial";
+
+// displayWeather(cityName, fetchUrl);
